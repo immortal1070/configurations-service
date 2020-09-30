6d0d3113daab1058eee92e3ab7598b4c8c52a195
Sergey Malahov <sergey.malahov@brandmaker.com>
2020-09-30 21:32:58 +0200

    Rename scripts

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-sh $DIR/scripts/shutdown.sh && sh $DIR/scripts/startDev.sh && sh $DIR/scripts/packageDev.sh
+sh $DIR/scripts/shutdown.sh && sh $DIR/scripts/startDev.sh && sh $DIR/scripts/package-dev-minimal.sh
